6d565c51668e098c0002f704e607be6f25745eba
forgivenprincess <amelia.psarmiento@gmail.com>
2022-11-25 03:43:19 +0100

    Update classroommanagers.js

--- classroommanagers.js
+++ classroommanagers.js
@@ -51,39 +51,12 @@ export async function displayOptions() {
             choice = await optionsAvailable();
 
         } catch (error) {
-            console.log(error)
+            console.log("\n\n Error unknown")
             process.exit(0)
         }
-    } while (optionsAvailable > 0 && optionsAvailable <= 18)
+   
 
-}
-
-/* 
-### OPCIONES
-### INDISPENSABLES
-0- Cerrar programa
-1- Mostrar en formato de tabla todos los alumnos.
-2- Mostrar por consola la cantidad de alumnos que hay en clase.
-3- Mostrar por consola todos los nombres de los alumnos.
-4- Eliminar el último alumno de la clase.
-5- Eliminar un alumno aleatoriamente de la clase.
-6- Mostrar por consola todos los datos de los alumnos que son chicas.
-7- Mostrar por consola el número de chicos y chicas que hay en la clase.
-8- Mostrar true o false por consola si todos los alumnos de la clase son chicas.
-9- Mostrar por consola los nombres de los alumnos que tengan entre 20 y 25 años.
-10- Añadir un alumno nuevo con los siguientes datos:
-11- Mostrar por consola el nombre de la persona más joven de la clase.
-12- Mostrar por consola la edad media de todos los alumnos de la clase.
-13- Mostrar por consola la edad media de las chicas de la clase.
-14- Añadir nueva nota a los alumnos. Por cada alumno de la clase, tendremos que calcular una nota de forma aleatoria(número entre 0 y 10) y añadirla a su listado de notas.
-15- Ordenar el array de alumnos alfabéticamente según su nombre.
-### OPTATIVOS
-16- Mostrar por consola el alumno de la clase con las mejores notas.
-17- Mostrar por consola la nota media más alta de la clase y el nombre del alumno al que pertenece.
-18- Añadir un punto extra a cada nota existente de todos los alumnos. Recordad que la nota máxima posible es 10. Si los alumnos aún no tienen registrada ninguna nota, les pondremos un 10.
-*/
-
-switch (optionsAvailable) {
+switch(choice) {
     case 0:
         "exit",
             console.log("\nThank you for using our Classroom Manager 2022. Good bye!");
@@ -173,4 +146,10 @@ switch (optionsAvailable) {
             addPointsToAll(students);
         break;
 }
+
+
+} while (choice > 0 && choice <= 18)
+
+}
+
 displayOptions()
