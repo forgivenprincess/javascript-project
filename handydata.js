06080cdafadd20dff10de0709fff418c4bab609c
forgivenprincess <amelia.psarmiento@gmail.com>
2022-11-25 03:23:25 +0100

    delete menu due to integration in management

--- handydata.js
+++ handydata.js
@@ -19,31 +19,9 @@ export const showOptions = () => {
   console.log(optionsText)
 }
 
-const optionsText = '\n\nEscribe un número de la siguiente lista para acceder a su funcionalidad correspondiente:\nPara salir de la aplicación:\n- Escoge 0 o cualquier número distinto al de las opciones.\n\n1 - Tabla  de todos los alumnos.\n2 - Número de alumnos que hay en clase.\n3 - Nombres de todos los alumnos.\n4 - Elimina al último alumno de la lista.\n5 - Elimina a un alumno aleatoriamente.\n6 - Datos de las alumnas.\n7 - Número de chicos y chicas que hay en la clase.\n8 - ¿Todos los alumnos son chicas?(true/false).\n9 - Nombres de los alumnos entre 20 y 25 años.\n10 -Añade un alumno nuevo con los siguientes datos:\n   - Nombre aleatorio.\n   - Edad aleatoria entre 20 y 50 años.\n   - Género aleatorio (consecuente con nombre).\n   - Calificaciones - vacío.\n11 - Nombre de la persona más joven de la clase.\n12 - Edad media de todos los alumnos de la clase.\n13 - Edad media de las chicas de la clase.\n14 - Añade nueva nota aleatoria al listado de cada alumno(entre 0 y 10).\n15 - Ordena alfabéticamente a los alumnos según su nombre.\n16 - Alumno de la clase con mayor sumatorio de notas.\n17 - Nota media más alta de la clase y alumno al que pertenece.\n18 - Añade un punto extra a cada nota existente de todos los alumnos.\n(nota máxima posible: 10. Si aún no hay registro, primera nota: 10.)\n\n'
-
 export const isInt = (num) => {
   return !Number.isNaN(num)
 }
 
 
-export const printMenu = {
-  0: 'Press to exit.',
-  1: 'Show alumni display table',
-  2: 'Show amount of alumni',
-  3: 'Show name of all alumni',
-  4: 'Delete last added student',
-  5: 'Delete random student',
-  6: 'Show all female alumni data',
-  7: 'Show amount of female and male alumni',
-  8: 'Check if all students are female',
-  9: 'Display alumni between ages 20 and 25',
-  10: 'Add a random blank student ',
-  11: 'Display youngest student',
-  12: 'Display average alumni age',
-  13: 'Display average female alumni age',
-  14: 'Add new random grade to all students',
-  15: 'Sort alphabetically students by their name',
-  16: 'Show best student/s (all rates added)',
-  17: 'Show the best student according to their average rate',
-  18: 'Add an extra point to each rate for all alumni. Set rate as 10 if there is no existing rate',
-}
+export const optionsText =  '\n\nSelect functionality by choosing a number out from the following commands \n\n0: Press to exit \n\n1: Show alumni display table\n\n2: Show amount of alumni \n\n3: Show name of all alumni\n\n4: Delete last added student\n\n5: Delete random studen\n\n6: Show all female alumni data \n\n7: Show amount of female and male alumni \n\n8: Check if all students are female \n\n9: Display alumni between ages 20 and 25 \n\n10: Add a random blank student\n\n11: Display youngest student \n\n12: Display average alumni age \n\n13: Display average female alumni age \n\n14: Add new random grade to all students \n\n15: Sort alphabetically students by their name \n\n16: Show best student/s (all rates added) \n\n17: Show the best student according to their average rate \n\n18: Add an extra point to each rate for all alumni. Set rate as 10 if there is no existing rate'
--- menu.js
+++
@@ -1,3 +0,0 @@
-import {printMenu} from './handydata.js'
-
-printMenu()
